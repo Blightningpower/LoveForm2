894c6bee7f3c2e5198426df5b01ba74dd666c5f6
Bavli Armanyous <bavli898@gmail.com>
2024-01-23 23:51:06 +0100

    new update

--- process_form.php
+++ process_form.php
@@ -3,15 +3,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $email = $_POST["email"];
     $message = $_POST["message"];
-    $to = "bavli2002@hotmail.com"; // Replace with your email address
-    $subject = "Contact Form Submission from $name";
-    $headers = "From: $email";
+    $to = "bavli2002@hotmail.com";
+    $subject = "New Contact Form Submission";
 
-    // Send the email
-    if (mail($to, $subject, $message, $headers)) {
-        echo "Thank you for your message. We'll get back to you soon!";
-    } else {
-        echo "Sorry, there was an error sending your message. Please try again later.";
-    }
+    $headers = "From: $email" . "\r\n" .
+               "Reply-To: $email" . "\r\n" .
+               "X-Mailer: PHP/" . phpversion();
+
+    mail($to, $subject, $message, $headers);
+
+    
+    exit();
 }
 ?>
